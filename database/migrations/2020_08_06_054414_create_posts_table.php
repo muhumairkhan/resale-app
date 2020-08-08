0cc9339eaf1d0793fe2c4cd6ce1e1f0d44393915
umairkhangt <muhumairkhan.gt@gmail.com>
2020-08-08 08:37:26 +0500

    User and post schema updated

--- database/migrations/2020_08_06_054414_create_posts_table.php
+++ database/migrations/2020_08_06_054414_create_posts_table.php
@@ -15,6 +15,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->string('title');
+            $table->text('description');
+            $table->float('price');
             $table->timestamps();
         });
     }

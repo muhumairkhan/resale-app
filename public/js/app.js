333b944af6760efa839f07de7316808fdd940967
umairkhangt <muhumairkhan.gt@gmail.com>
2020-08-09 18:30:41 +0500

    Remove js file comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,4 @@
 $(document).ready(function (){
-
-    // $('#post-dlt').click(function (e){
-    //     e.preventDefault();
-    //
-    //     post_id = $(this).attr('data-post-id');
-    //
-    //     $.ajax({
-    //         url: '/ad/'+post_id,
-    //         type: 'DELETE',
-    //         success: function(result) {
-    //             alert('Ad has been deleted.');
-    //
-    //             window.location.href = "/";
-    //         }
-    //     });
-    // });
+    
 
 });
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,4 @@
 $(document).read(function (){
 
-    $('.post-dlt').click(function (){
-        alert('button clicked');
-    });
 
 });

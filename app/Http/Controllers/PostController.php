2bb357ed194190580c1e9e1cf8f31a13063558bc
umairkhangt <muhumairkhan.gt@gmail.com>
2020-08-11 10:13:34 +0500

    Added authentication on create, update, edit and delete route for PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,13 @@ use MongoDB\Driver\Session;
 
 class PostController extends Controller
 {
+    
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index','show']]);
+
+    }
+
     /**
      * Display a listing of the resource.
      *
